72acf6aa305c1a6001f397faf8f38f3e2336789e
tien-tq200540 <tqtien157202@gmail.com>
2024-08-08 23:57:49 +0700

    Add FIND command ver1.0 and some small updates

---
+++ nFIND/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace nFIND
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string/paragraph you want to find: "); var inputString = Console.ReadLine() ?? string.Empty;
+            Console.Write("Enter the file path: "); var inputPath = Console.ReadLine();
+
+            var inputFilePath = $"{inputPath}";
+
+            if (!File.Exists(inputFilePath))
+            {
+                Console.WriteLine($"File not found - {inputPath}");
+            } else
+            {
+                var fileInfo = new FileInfo(inputFilePath);
+                Console.WriteLine($"---------- {fileInfo.Name}");
+                using var instream = fileInfo.OpenRead();
+                using var streamRreader = new StreamReader(instream);
+                var line = string.Empty;
+                while ((line = streamRreader.ReadLine()) != null)
+                {
+                    if (line.Contains(inputString))
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }  
+        }
+    }
+}
--- nTREE/obj/Debug/net8.0/nTREE.AssemblyInfo.cs
+++ nTREE/obj/Debug/net8.0/nTREE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("nTREE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fed3d57680e718299f811a39215ff504b598059c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+848e7c4e9b37cc44f1f5cf00ef4c1fa556e3a9f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("nTREE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("nTREE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
